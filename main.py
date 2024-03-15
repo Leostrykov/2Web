ca5cc9154b3122909ffb085c0a70f541fe29a00d
Leonid Strukov <leostrukov@ya.ru>
2024-03-16 00:08:51 +0300

    Сделал задание 3

--- main.py
+++ main.py
@@ -15,5 +15,14 @@ def training(prof):
     return render_template('training.html', title='Симуляторы', prof=prof)
 
 
+@app.route('/list_prof/<list_type>')
+def list_prof(list_type):
+    professions = ['инженер-исследователь', 'пилот', 'строитель', 'экзобиолог', 'врач', 'инженер по терраформированию',
+                   'климатолог', 'специалист по радиационной защите', 'астрогеолог', 'гляциолог',
+                   'инженер жизнеобеспечения', 'метеоролог', 'оператор марсохода', 'киберинженер', 'штурман',
+                   'пилот дронов']
+    return render_template('professions.html', title='Профессии', list_type=list_type, profs=professions)
+
+
 if __name__ == '__main__':
     app.run(port=8080, host='127.0.0.1')
