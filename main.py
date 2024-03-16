7b0fb9ce36eefec25d588388be48d03c01bd4aae
Leonid Strukov <leostrukov@ya.ru>
2024-03-16 21:17:41 +0300

    Сделал задание 5

--- main.py
+++ main.py
@@ -1,8 +1,19 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request, redirect
 from config import SECRET_KEY
+from flask_wtf import FlaskForm
+from wtforms import StringField, PasswordField, SubmitField
+from wtforms.validators import DataRequired
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = SECRET_KEY
+
+
+class LoginForm(FlaskForm):
+    id_aust = StringField('id астронавта', validators=[DataRequired()])
+    password_aust = PasswordField('Пароль астронавта', validators=[DataRequired()])
+    id_cap = StringField('id капитана', validators=[DataRequired()])
+    password_cap = PasswordField('Пароль капитана', validators=[DataRequired()])
+    submit = SubmitField('Доступ')
 
 
 @app.route('/index/<title>')
@@ -38,6 +49,19 @@ def answer():
         'ready': 'Да'
     }
     return render_template('auto_answer.html', **ans)
+
+
+@app.route('/login', methods=['POST', 'GET'])
+def login():
+    form = LoginForm()
+    if form.validate_on_submit():
+        return redirect('/success')
+    return render_template('login_page.html', title='Вход', form=form)
+
+
+@app.route('/success')
+def success():
+    return 'Форма отправлена'
 
 
 if __name__ == '__main__':
