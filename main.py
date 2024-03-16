f734ced6eefecc0e68ae3280c406e2bf0fabc029
Leonid Strukov <leostrukov@ya.ru>
2024-03-16 23:36:10 +0300

    Сделал задание 7

--- main.py
+++ main.py
@@ -1,8 +1,9 @@
 from flask import Flask, render_template, request, redirect
 from config import SECRET_KEY
 from flask_wtf import FlaskForm
-from wtforms import StringField, PasswordField, SubmitField
+from wtforms import StringField, PasswordField, SubmitField, FileField
 from wtforms.validators import DataRequired
+import os
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = SECRET_KEY
@@ -14,6 +15,11 @@ class LoginForm(FlaskForm):
     id_cap = StringField('id капитана', validators=[DataRequired()])
     password_cap = PasswordField('Пароль капитана', validators=[DataRequired()])
     submit = SubmitField('Доступ')
+
+
+class LoadFileForm(FlaskForm):
+    photo = FileField('Загрузите фото', validators=[DataRequired()])
+    submit = SubmitField('Отправить')
 
 
 @app.route('/index/<title>')
@@ -68,6 +74,20 @@ def success():
 def distribution():
     return render_template('distribution.html', title='Пассажиры', passengers=['Ридди Скот',
                                                                                'Энди Уир', 'Марк Уотни'])
+
+
+images = ['image-1.jpg', 'image-2.jpg', 'image-3.jpg']
+
+
+@app.route('/galery', methods=['GET', 'POST'])
+def galery():
+    form = LoadFileForm()
+    if form.validate_on_submit():
+        f = form.photo.data
+        f.save(os.path.join('static/img', f.filename))
+        images.append(f.filename)
+        return redirect('/galery')
+    return render_template('images.html', title='Галерея', images=images, form=form)
 
 
 if __name__ == '__main__':
